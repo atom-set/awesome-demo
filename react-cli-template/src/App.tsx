61db3197cfe63d5ccacef42febcc3d141acacd86
xiao.geng <xiao.geng@relxtech.com>
2024-01-17 10:40:48 +0800

    feat: init

---
+++ react-cli-template/src/App.tsx
@@ -0,0 +1,42 @@
+import React, { Suspense } from 'react';
+import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { lazy } from 'react'
+import Layout from "./components/Layout";
+
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Suspense
+          fallback={
+            <div
+              style={{
+                textAlign: 'center',
+                marginTop: 200
+              }}
+            >
+              loading...
+            </div>
+          }
+        >
+          <Routes>
+            <Route element={<Layout />} >
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-cli-template/src/components/Layout.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function Layout() {
+  const [pagePath, setPagePath] = useState(window.location.pathname);
+  const testMenuItems = [
+    // 联动
+    {
+      title: 'Home',
+      href: '/home',
+    },
+    {
+      title: 'About',
+      href: '/about',
+    },
+
+  ];
+
+  const handleClick = () => {
+    setPagePath(window.location.pathname)
+  };
+
+  return (
+    <div className='page'>
+      {
+        pagePath === '/' && <nav>
+          <ul className="menu-list">
+            {testMenuItems.map(({ href, title }) => (
+              <li className='menu-item' key={title} onClick={handleClick}>
+                <NavLink to={href}>
+                  <p className={'text-black'}>{title}</p>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      }
+
+      <main>
+        <div className="container">
+          <Outlet />
+        </div>
+      </main>
+      {
+        pagePath !== '/' && <a href="/" className="back">返回</a>
+      }
+    </div>
+  );
+}
